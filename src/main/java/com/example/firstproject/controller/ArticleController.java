73123ff91f907cecc16b0f463ba2c637fbee5649
ttochi3 <ttochi3795@gmail.com>
2023-07-24 21:09:37 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/article/new")
+    public String newArticleForm() {
+        return "/article/new";
+    }
+
+    @PostMapping("/article/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+
+        // 1. DTO를 엔티티로 변환.
+        Article article = form.toEntity();
+
+        // 2. 리파지터리로 엔티티를 DB에 저장.
+        Article saved = articleRepository.save(article);
+
+        return "";
+    }
+}
