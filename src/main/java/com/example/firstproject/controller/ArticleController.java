ebc391765f20dff86fd607335baab3fc6a8735c1
ttochi3 <ttochi3795@gmail.com>
2023-08-02 22:04:27 +0900

    lombok 설치 후 Refactoring - firstproject/controller/ArticleController.java - firstproject/dto/ArticleForm.java - firstproject/Article.java

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -20,14 +22,16 @@ public class ArticleController {
 
     @PostMapping("/article/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        //System.out.println(form.toString());
+        log.info(form.toString());
 
         // 1. DTO를 엔티티로 변환.
         Article article = form.toEntity();
 
         // 2. 리파지터리로 엔티티를 DB에 저장.
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
