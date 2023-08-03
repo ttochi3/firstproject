9bf7524cc26ec36c002010b5bcb7dce8ca5433da
ttochi3 <ttochi3795@gmail.com>
2023-08-03 22:42:14 +0900

    변경

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +17,12 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
-    @GetMapping("/article/new")
+    @GetMapping("/articles/new")
     public String newArticleForm() {
-        return "/article/new";
+        return "/articles/new";
     }
 
-    @PostMapping("/article/create")
+    @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         //System.out.println(form.toString());
         log.info(form.toString());
@@ -34,12 +35,17 @@ public class ArticleController {
         //System.out.println(saved.toString());
         log.info(saved.toString());
 
-        return "";
+        return "/articles/create";
     }
 
     @GetMapping("/articles/{id}") //데이터 조회 요청 접수
-    public String show(@PathVariable Long id) {
+    public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id); //id를 잘 받았는지 확인하는 로그 찍기.
-        return "";
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
