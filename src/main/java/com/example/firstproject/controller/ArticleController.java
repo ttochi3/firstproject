970dd4aaac350435b814c6c01a8876e8c24c6a4c
ttochi3 <ttochi3795@gmail.com>
2023-08-06 15:34:31 +0900

    Update ArticleController.java

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -70,4 +70,21 @@ public class ArticleController {
         // 뷰 페이지 설정하기.
         return "/articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) { // 매개변수로 DTO 받아오기
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if(target!=null) {
+            articleRepository.save(articleEntity);
+        }
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
