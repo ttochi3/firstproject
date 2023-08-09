2abb3a5e23c3304f7ea0794aa025896cb498f240
ttochi3 <ttochi3795@gmail.com>
2023-08-09 22:33:36 +0900

    변경

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -49,15 +49,29 @@ public class ArticleApiController {
         // 2. 타깃 조회하기
         Article target = articleRepository.findById(id).orElse(null);
         // 3. 잘못된 요청 처리하기
-        if(target == null || !id.equals(article.getId())) {
+        if (target == null || !id.equals(article.getId())) {
             // 400, 잘못된 요청 응답!
             log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         // 4. 업데이트 및 정상 응답(200)하기
-        Article updated = articleRepository.save(article);
+        target.patch(article);
+        Article updated = articleRepository.save(target);
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 
     // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id){
+        // 1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 2. 잘못된 요청 처리하기
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 3. 대상 삭제하기
+        articleRepository.delete(target);
+        //return ResponseEntity.status(HttpStatus.OK).body(null);
+        return ResponseEntity.status(HttpStatus.OK).build(); //HTTP응답의 body가 없는 ResponseEntity 객체를 생성, body(null)의 결과와 동일함.
+    }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -22,4 +22,9 @@ public class Article {
 
     public Article(Object o, String title, String content) {
     }
+
+    public void patch(Article article) {
+        if(article.title != null) this.title = article.title;
+        if(article.content != null) this.content = article.content;
+    }
 }
