1ba1255319c9f81630acdcdbfa67dfbcfb1c6beb
ttochi3 <ttochi3795@gmail.com>
2023-08-15 21:36:09 +0900

    JpaRepository

--- src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -55,9 +55,16 @@ class CommentRepositoryTest {
         /* Case 1: "Park"의 모든 댓글 조회 */
         {
             // 1. 입력 데이터 준비
+            String nickname = "Park";
             // 2. 실제 데이터
+            List<Comment> comments = commentRepository.findByNickName(nickname);
             // 3. 예상 데이터
+            Comment a = new Comment(1L, new Article(4L, "당신의 인생 영화는?", "댓글 고"), nickname,"굿 윌 헌팅");
+            Comment b = new Comment(4L, new Article(5L, "당신의 소울 푸드는?", "댓글 고고"), nickname,"치킨");
+            Comment c = new Comment(7L, new Article(6L, "당신의 취미는?", "댓글 고고고"), nickname,"조깅");
+            List<Comment> expected = Arrays.asList(a, b, c);
             // 4. 비교 및 검증
+            assertEquals(expected.toString(), comments.toString(), "Park의 모든 댓글을 출력!@");
         }
     }
 }
